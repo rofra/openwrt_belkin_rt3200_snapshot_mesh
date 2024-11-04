77495a2c8d5e8a272df3b162e3564ae51c2b5160
Rodolphe FRANCESCHI <fedorage@gmail.com>
2024-11-04 21:36:40 +0100

    Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# belkin_rt3200_snapshot_mesh
-A script to initialize a fully fonctionnal Belkin RT3200 Mesh network capable
+# OpenWRT Belkin RT3200 Snapshot Mesh (802.11s)
+A simple script to initialize a fully fonctionnal Belkin RT3200 Mesh network capable for Openwrt. 
+
+## Introduction
+One shot script to configure a fresh SNAPSHOT installed operating system for Mesh networks.
+
+*What it does :*
++ Fix the WIFI configuration
++ Remove the "mbedtls" and replace them with the openssl ones
++ Install necessary Kernel Modules (kmods) for networking and wireless card
++ Install Mesh tools
++ Install Luci with package manager on HTTP + HTTPS (with redirection)
++ Activate WEP and install bridger for Dumb Access Point
++ Enable software and hardware network buffering
+
+## Usage
+Note: Snapshot releases do not have luci, you need to connect with SSH as root to begin the installation process
+
+```bash
+# Allow scp 
+ssh root@192.168.1.1 "opkg update && opkg install openssh-sftp-server"
+# Copy the installation script
+scp install.sh root@192.168.1.1:/tmp/
+# Launch it remotly
+ssh root@192.168.1.1 "chmod u+x /tmp/install.sh && /tmp/install.sh"
+```
+
+### Notes
+This script will only work with recent snapshots like the *openwrt-mediatek-mt7622-linksys_e8450-ubi-squashfs-sysupgrade.itb* in the repository (2024-11-04 release)
---
+++ install.sh
@@ -0,0 +1,102 @@
+#/bin/env ash
+set -e
+
+# Script for installing a ready to go Belkin RT2000 with full mesh support with Openssh
+# 
+# Tested with version "SNAPSHOT openwrt-24.10 branch 24.298.52515~e34c268" gathered on the 4th of November 2024. 
+# sha256sum: 7b8b840a81a89fefa5754f355725c34bb7c7c2cdb57d39972bba127e151bb566
+#
+
+# Prepare
+opkg update
+
+# Fix Wifi configuration
+echo "config wifi-device 'radio0'
+	option type 'mac80211'
+	option path 'platform/18000000.wmac'
+	option channel 'auto'
+	option band '2g'
+	option htmode 'HT40'
+	option txpower '20'
+	option cell_density '0'
+
+config wifi-iface 'default_radio0'
+	option device 'radio0'
+	option network 'lan'
+	option mode 'ap'
+	option ssid 'OpenWRT'
+	option encryption 'none'
+	option disabled '1'
+
+config wifi-device 'radio1'
+	option type 'mac80211'
+	option path '1a143000.pcie/pci0000:00/0000:00:00.0/0000:01:00.0'
+	option cell_density '0'
+	option country 'US'
+	option htmode 'HE40'
+	option band '5g'
+	option channel 'auto'
+	option disabled '1'
+
+config wifi-iface 'default_radio1'
+	option device 'radio1'
+	option network 'lan'
+	option mode 'ap'
+	option ssid 'OpenWRT'
+	option encryption 'none'
+	option disabled '1'
+
+" > /etc/config/wireless
+
+# Replace libustream by openssl one
+opkg --cache /tmp/ --download-only install libustream-openssl20201210
+opkg remove libustream-mbedtls20201210
+opkg --cache /tmp/ install libustream-openssl20201210
+
+# Install Luci
+opkg install luci
+
+# Install Luci HTTPS openssl
+opkg update
+opkg install luci-ssl-openssl
+/etc/init.d/uhttpd restart
+uci set uhttpd.main.redirect_https=1
+uci commit uhttpd
+service uhttpd reload
+
+# Remove "mbedtls" related packages
+opkg remove wpad-basic-mbedtls
+opkg remove libmbedtls21
+
+# Install Openssl and Mesh
+opkg update
+opkg install wpad-mesh-openssl
+opkg install mesh11sd
+
+# Install required kernel modules
+opkg update
+opkg install kmod-mt7915-firmware
+opkg install kmod-nft-bridge
+
+# Enable WED
+echo 'options mt7915e wed_enable=Y' >> /etc/modules.conf
+# Install Bridger for WED + Dumb Access Point
+opkg update
+opkg install bridger
+
+# Enable Buffering
+{ head -n 5 /etc/config/firewall; echo -e "\toption flow_offloading\t'1'"; echo -e "\toption flow_offloading_hw\t'1'"; tail -n +6 /etc/config/firewall; } > /tmp/firewall
+mv -f /tmp/firewall /etc/config/firewall
+/etc/init.d/firewall restart
+
+# Install Luci package manager
+opkg install luci-app-opkg
+
+# BONUS: Install Statistics
+# opkg install luci-app-statistics
+
+# Reboot an Enjoy
+reboot
+
+
+
